e0062bc55ec91d29c052c4c4443eb84b660b487d
Chueawangkham <thibess091@gmail.com>
2022-06-27 06:58:51 +0700

    update

--- lib/profile.dart
+++ lib/profile.dart
@@ -27,40 +27,36 @@ class AboutMe extends StatelessWidget {
     Widget Name() {
       return Column(
         children: <Widget>[
-          Column(children: const [
-            Text(
-              'Thibet Chueawangkham{',
-              style: TitleFont,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Thibet Chueawangkham{',
+                  style: TitleFont,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '/ /   FontEnd Developer (Flutter)',
+                  style: ContentFont,
+                ),
+                Text(
+                  'About me  {',
+                  style: SubTitleFont,
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                  child: Text(
+                    '       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ',
+                    style: ContentFont,
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '/ /   FontEnd Developer (Flutter)',
-              style: ContentFont,
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 30, 280, 0),
-              child: Text(
-                'About me  {',
-                style: SubTitleFont,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                '       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ',
-                style: ContentFont,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 350, 0),
-              child: Text(
-                '}',
-                style: SubTitleFont,
-              ),
-            ),
-          ]),
+          ),
         ],
       );
     }
@@ -68,20 +64,15 @@ class AboutMe extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF272727),
       body: SafeArea(
-        child: Container(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Name(),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-            ),
+              Name(),
+            ],
           ),
         ),
       ),
